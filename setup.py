1bac424728d5ef73165e29b2cae1fd86541b7be6
gmatuck <gmatuck@gmail.com>
2018-07-10 16:02:57 -0300

    commit 1 (inicial)

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Descrição do projeto....',
+    author='Gustavo Matuck',
+    license='MIT',
+)
